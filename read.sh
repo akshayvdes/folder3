e628b0ba750b397a5367cfd45524cf81cc4d0405
akshayvdes <vd.akshaykumar@gmail.com>
2021-12-18 05:16:10 +0000

    added 2file

---
+++ read.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "enter the name of the employee"
+read name
+echo "enter the disgnation of employee"
+read designation
+echo "enter the age"
+read age
+echo "enter the company name"
+read company
+
+echo "name of employee is $name"
+echo "designationof employee is $designation"
+echo "age of the employe is $age"
+echo "employer name is $company"
+
